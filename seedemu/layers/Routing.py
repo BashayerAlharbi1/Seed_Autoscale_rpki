72995f7af43fd8145ef1edaa5a7e46aebe7f60b1
BashayerAlharbi <baal7013@colorado.edu>
2023-04-11 01:43:25 -0600

    updates

--- seedemu/layers/Routing.py
+++ seedemu/layers/Routing.py
@@ -111,9 +111,9 @@ class Routing(Layer):
         """
         #KO
         #Removed following 3 lines and included them in base image build instead. Used to speed up deployments.
-        #node.addBuildCommand('mkdir -p /usr/share/doc/bird2/examples/')
-        #node.addBuildCommand('touch /usr/share/doc/bird2/examples/bird.conf')
-        #node.addBuildCommand('apt-get update && apt-get install -y --no-install-recommends bird2')
+        node.addBuildCommand('mkdir -p /usr/share/doc/bird2/examples/')
+        node.addBuildCommand('touch /usr/share/doc/bird2/examples/bird.conf')
+        node.addBuildCommand('apt-get update && apt-get install -y --no-install-recommends bird2')
 
     def configure(self, emulator: Emulator):
         reg = emulator.getRegistry()
